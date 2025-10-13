06a9a098ba6225a1f65fd7efedcc8f1e30712c50
Andrew Capaldo <andycapaldo2@gmail.com>
2025-10-13 15:05:37 -0600

    Added Dynamic packages for EVM and SOL, configured env and example env

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
 
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 const App = () => {
+  const envID = import.meta.env.VITE_DYNAMIC_ENVIRONMENT_ID;
+
   return (
-    <div>
-      Dynamic Demo
-    </div>
+    <DynamicContextProvider
+      settings={{
+        environmentId: envID,
+        walletConnectors: [EthereumWalletConnectors,SolanaWalletConnectors],
+        multiWallet: true,
+      }}
+    >
+      <DynamicWidget />
+    </DynamicContextProvider>
   )
 }
 
