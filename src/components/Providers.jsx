9aff23b9b92a05da6e47a229bde78bd558a138ef
Andrew Capaldo <andycapaldo2@gmail.com>
2025-10-14 14:25:50 -0600

    Added wagmi providers for minting nfts

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,10 +1,26 @@
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createConfig, WagmiProvider } from "wagmi";
+import { http } from 'viem';
+import { baseSepolia } from "wagmi/chains";
+
+const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 const Providers = ({ children }) => {
   const envID = import.meta.env.VITE_DYNAMIC_ENVIRONMENT_ID;
+
   return (
     <DynamicContextProvider
       theme="dark"
@@ -18,7 +34,13 @@ const Providers = ({ children }) => {
         multiWallet: true,
       }}
     >
-      {children}
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector config={wagmiConfig}>
+            {children}
+          </DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
     </DynamicContextProvider>
   );
 };
