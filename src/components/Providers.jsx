6d51085c9cd9d74aa590631aca4ddb5c5515741f
Andrew Capaldo <andycapaldo2@gmail.com>
2025-10-14 12:33:37 -0600

    Added gas sponsorship/account abstraction through Zerodev

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,6 +1,7 @@
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
 
 const Providers = ({ children }) => {
   const envID = import.meta.env.VITE_DYNAMIC_ENVIRONMENT_ID;
@@ -9,7 +10,11 @@ const Providers = ({ children }) => {
       theme="dark"
       settings={{
         environmentId: envID,
-        walletConnectors: [EthereumWalletConnectors, SolanaWalletConnectors],
+        walletConnectors: [
+          EthereumWalletConnectors,
+          SolanaWalletConnectors,
+          ZeroDevSmartWalletConnectors,
+        ],
         multiWallet: true,
       }}
     >
