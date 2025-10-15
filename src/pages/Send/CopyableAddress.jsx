6babbf36ba24f56b5b489b8694094fb91ca55a88
Andrew Capaldo <andycapaldo2@gmail.com>
2025-10-14 20:41:50 -0600

    Added tx sending instructions; added copyable address component and passed in a base sepolia address for demo

---
+++ src/pages/Send/CopyableAddress.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const CopyableAddress = ({ address }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = async () => {
+    if (isCopied) return;
+
+    try {
+      await navigator.clipboard.writeText(address);
+
+      setIsCopied(true);
+
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    } catch (err) {
+      console.error("Failed to copy address: ", err);
+    }
+  };
+
+  return (
+    <div
+      onClick={handleCopy}
+      className="inline-flex items-center gap-2 p-2 mt-2 border rounded-lg cursor-pointer hover:bg-cerulean/55 dark:hover:bg-gray-700"
+    >
+      <span className="font-mono">
+        {`${address.substring(0, 6)}...${address.substring(
+          address.length - 4
+        )}`}
+      </span>
+
+      {isCopied ? (
+        <span className="text-green-500">Copied!</span>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+          <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zM-1 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default CopyableAddress;
--- src/pages/Send/Faucet.jsx
+++ src/pages/Send/Faucet.jsx
@@ -1,4 +1,5 @@
 import * as faucetService from "../../lib/hooks";
+import CopyableAddress from "./CopyableAddress";
 
 const Faucet = () => {
   const { isLoading, txHash, error, mintTokens } = faucetService.useMint();
@@ -35,16 +36,36 @@ const Faucet = () => {
 
       {/* Transaction Status */}
       {txHash && (
-        <div className="text-green-500">
-          <p>Success! You received 100 USDC.</p>
-          <a
-            href={`https://base-sepolia.blockscout.com/tx/${txHash}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="underline"
-          >
-            View on Blockscout
-          </a>
+        <div>
+          <div className="text-green-500">
+            <p>Success! You received 100 USDC.</p>
+            <a
+              href={`https://base-sepolia.blockscout.com/tx/${txHash}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              View on Blockscout
+            </a>
+          </div>
+          <div className="mt-4">
+            <h2 className="text-xl font-bold">Send a Transaction</h2>
+            <p className="my-4 text-wrap">
+              Now that you have some (fake) cash to throw around, follow the steps 
+              below to send some USDC to another address!
+            </p>
+            <ol>
+                <li className="mb-2">1. Click the "Send" button on the below Dynamic Widget</li>
+                <li className="mb-2">2. Select Fake USDC as the asset to send</li>
+                <li className="mb-2">3. Enter an amount to send</li>
+                <li className="mb-2">4. Enter a recipient address (click to copy a real wallet below!)</li>
+                <li className="mb-2">5. Click on the preview transaction button</li>
+                <li className="mb-2">6. If everything looks good, click confirm!</li>
+            </ol>
+            <div>
+                <CopyableAddress address="0xd0E90E241DB88d39362e0C3427cd1f53d98aC910"/>
+            </div>
+          </div>
         </div>
       )}
       {error && <p className="text-red-500">Error: {error}</p>}
